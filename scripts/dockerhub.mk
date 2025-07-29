6f7da74486f5c8243541190c72817a5176f40921
Moritz Marquardt <git@momar.de>
2025-07-29 10:23:35 +0200

    Change import path & release links to the forked repository

--- scripts/dockerhub.mk
+++ scripts/dockerhub.mk
@@ -25,36 +25,36 @@ dockerhub:
 	docker buildx create --name=builder --use
 
 	echo "$$DOCKERFILE_DOCKERHUB" | docker buildx build . -f - --build-arg VERSION=$(VERSION) \
-	--push -t aler9/landiscover:$(VERSION)-amd64 --build-arg OPTS="GOOS=linux GOARCH=amd64" --platform=linux/amd64
+	--push -t momar/landiscover:$(VERSION)-amd64 --build-arg OPTS="GOOS=linux GOARCH=amd64" --platform=linux/amd64
 
 	echo "$$DOCKERFILE_DOCKERHUB" | docker buildx build . -f - --build-arg VERSION=$(VERSION) \
-	--push -t aler9/landiscover:$(VERSION)-armv6 --build-arg OPTS="GOOS=linux GOARCH=arm GOARM=6" --platform=linux/arm/v6
+	--push -t momar/landiscover:$(VERSION)-armv6 --build-arg OPTS="GOOS=linux GOARCH=arm GOARM=6" --platform=linux/arm/v6
 
 	echo "$$DOCKERFILE_DOCKERHUB" | docker buildx build . -f - --build-arg VERSION=$(VERSION) \
-	--push -t aler9/landiscover:$(VERSION)-armv7 --build-arg OPTS="GOOS=linux GOARCH=arm GOARM=7" --platform=linux/arm/v7
+	--push -t momar/landiscover:$(VERSION)-armv7 --build-arg OPTS="GOOS=linux GOARCH=arm GOARM=7" --platform=linux/arm/v7
 
 	echo "$$DOCKERFILE_DOCKERHUB" | docker buildx build . -f - --build-arg VERSION=$(VERSION) \
-	--push -t aler9/landiscover:$(VERSION)-arm64v8 --build-arg OPTS="GOOS=linux GOARCH=arm64" --platform=linux/arm64/v8
+	--push -t momar/landiscover:$(VERSION)-arm64v8 --build-arg OPTS="GOOS=linux GOARCH=arm64" --platform=linux/arm64/v8
 
-	docker manifest create aler9/landiscover:$(VERSION) \
-	$(foreach ARCH,amd64 armv6 armv7 arm64v8,aler9/landiscover:$(VERSION)-$(ARCH))
-	docker manifest push aler9/landiscover:$(VERSION)
+	docker manifest create momar/landiscover:$(VERSION) \
+	$(foreach ARCH,amd64 armv6 armv7 arm64v8,momar/landiscover:$(VERSION)-$(ARCH))
+	docker manifest push momar/landiscover:$(VERSION)
 
-	docker manifest create aler9/landiscover:latest-amd64 aler9/landiscover:$(VERSION)-amd64
-	docker manifest push aler9/landiscover:latest-amd64
+	docker manifest create momar/landiscover:latest-amd64 momar/landiscover:$(VERSION)-amd64
+	docker manifest push momar/landiscover:latest-amd64
 
-	docker manifest create aler9/landiscover:latest-armv6 aler9/landiscover:$(VERSION)-armv6
-	docker manifest push aler9/landiscover:latest-armv6
+	docker manifest create momar/landiscover:latest-armv6 momar/landiscover:$(VERSION)-armv6
+	docker manifest push momar/landiscover:latest-armv6
 
-	docker manifest create aler9/landiscover:latest-armv7 aler9/landiscover:$(VERSION)-armv7
-	docker manifest push aler9/landiscover:latest-armv7
+	docker manifest create momar/landiscover:latest-armv7 momar/landiscover:$(VERSION)-armv7
+	docker manifest push momar/landiscover:latest-armv7
 
-	docker manifest create aler9/landiscover:latest-arm64v8 aler9/landiscover:$(VERSION)-arm64v8
-	docker manifest push aler9/landiscover:latest-arm64v8
+	docker manifest create momar/landiscover:latest-arm64v8 momar/landiscover:$(VERSION)-arm64v8
+	docker manifest push momar/landiscover:latest-arm64v8
 
-	docker manifest create aler9/landiscover:latest \
-	$(foreach ARCH,amd64 armv6 armv7 arm64v8,aler9/landiscover:$(VERSION)-$(ARCH))
-	docker manifest push aler9/landiscover:latest
+	docker manifest create momar/landiscover:latest \
+	$(foreach ARCH,amd64 armv6 armv7 arm64v8,momar/landiscover:$(VERSION)-$(ARCH))
+	docker manifest push momar/landiscover:latest
 
 	docker buildx rm builder
 	rm -rf $$HOME/.docker/manifests/*
